61643e9de4ceaca0e20bf82a88cc9c07e988c163
Jacob Ressler <jressler96@gmail.com>
2019-12-14 19:10:01 -0500

    added dropshadow to hunt image

--- src/gui/HuntImage.java
+++ src/gui/HuntImage.java
@@ -3,25 +3,26 @@ package gui;
 import javafx.scene.image.Image;
 
 public class HuntImage extends javafx.scene.image.ImageView {
-    
-    String baseURL = "https://serebii.net/Shiny/SWSH/";
-    
-    public HuntImage(String dexnum) {
-	dexnum = "810";
-	setImage(new Image(baseURL + dexnum + ".png"));
-    }
-    
-    public void changeImage(String dexnum) {
-	switch (dexnum.length()) {
-	case 1:
-	    dexnum = "00" + dexnum;
-	    break;
-	case 2:
-		dexnum = "0" + dexnum;
-	default:
-	    break;
+
+	String baseURL = "https://serebii.net/Shiny/SWSH/";
+
+	public HuntImage(String dexnum) {
+		dexnum = "810";
+		setImage(new Image(baseURL + dexnum + ".png"));
+		setStyle("-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,1), 10, .5, 0, 0);");
 	}
-	
-	setImage (new Image(baseURL +dexnum + ".png"));
-    }
+
+	public void changeImage(String dexnum) {
+		switch (dexnum.length()) {
+			case 1:
+				dexnum = "00" + dexnum;
+				break;
+			case 2:
+				dexnum = "0" + dexnum;
+			default:
+				break;
+		}
+
+		setImage(new Image(baseURL + dexnum + ".png"));
+	}
 }
